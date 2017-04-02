9a4c494d9a4a8dc9fc9a1c5a95d51f2a590703a7
gaadesuo <peppu4@gmail.com>
2017-04-02 22:12:55 +0900

    pip修正

---
+++ src/gpio_setting.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+__author__ = "gaa"
+__date__ = '$2017/04/02 :20:02$'
+
+import time
+import wiringpi as GPIO
+
+# GPIOの初期化
+GPIO.wiringPiSetupGpio()
+
+"""*****注意*****
+関数を呼び出す際にはpin番号の値を与える事。値はint
+値を忘れるとエラーなのでtry文を忘れない。
+*****"""
+
+
+"""
+
+関数
+
+"""
+
+
+def led1(interval , pin):
+    """
+        *****注意*****
+    関数を呼び出す際にはpin番号の値を与える事。
+    値を忘れるとエラーなのでtry文を忘れない。
+        ---GPIO出力---
+    与えられたpin NOのGPIO PINのLEDを関数に与えられた値/10秒間点滅させる
+    :param interval: int: 点滅間隔の設定。与えられたの/10感覚で点滅させる
+    :param pin: int: LEDのGPIO PIN NO
+    """
+    LED1 = pin
+    GPIO.pinMode(LED1, GPIO.OUTPUT)
+    GPIO.digitalWrite(LED1, GPIO.HIGH)
+    time.sleep(interval / 10)
+    GPIO.digitalWrite(LED1, GPIO.LOW)
+    time.sleep(interval / 10)
+
+
+def led2(intterval, pin):
+    """
+        *****注意*****
+    関数を呼び出す際にはpin番号の値を与える事。
+    値を忘れるとエラーなのでtry文を忘れない。
+        ---GPIO出力---
+    与えられたpin NOのGPIO PINのLEDを関数に与えられた値/10秒間点滅させる
+    :param interval: int: 点滅間隔の設定。与えられたの/10感覚で点滅させる
+    :param pin: int: LEDのGPIO PIN NO
+    """
+    LED2 = pin
+    GPIO.pinMode(LED2, GPIO.OUTPUT)
+    GPIO.digitalWrite(LED2, GPIO.HIGH)
+    time.sleep(intterval / 10)
+    GPIO.digitalWrite(LED2, GPIO.LOW)
+    time.sleep((intterval / 10))
+
+
+def led1and2(interval, pin1, pin2):
+    """
+        *****注意*****
+    関数を呼び出す際にはpin番号の値を与える事。
+    値を忘れるとエラーなのでtry文を忘れない。
+        ---GPIO出力---
+    与えられたpin NOのGPIO PINのLEDを関数に与えられた値/10秒間点滅させる
+    :param interval: int: 点滅間隔の設定。与えられたの/10感覚で点滅させる
+    :param pin1: int: 1つ目LEDの GPIO PIN NO
+    :param pin2: int: 2つ目LEDの GPIO PIN NO
+    """
+    LED1 = pin1
+    LED2 = pin2
+    GPIO.pinMode(LED1, GPIO.OUTPUT)
+    GPIO.pinMode(LED2, GPIO.OUTPUT)
+    GPIO.digitalWrite(LED1, GPIO.HIGH)
+    GPIO.digitalWrite(LED2, GPIO.HIGH)
+    time.sleep(interval / 10)
+    GPIO.digitalWrite(LED1, GPIO.LOW)
+    GPIO.digitalWrite(LED2, GPIO.LOW)
+    time.sleep(interval / 10)
--- src/ltika.py
+++ src/ltika.py
@@ -2,10 +2,12 @@
 __author__ = "gaa"
 __date__ = '$2017/04/02 :20:36$'
 
-import pin14_15led as LED
+import gpio_setting as LED_PIN
 
 count = 0
 interval = 0
+pin1 = 0
+pin2 = 0
 
 print("""入力された時間の1/10秒ごと入力された回数LEDを点滅させます。
 点滅は片方ずつ1回毎点滅し最後に両方同時に点滅で1ループです。""")
@@ -18,8 +20,8 @@ while True:
         continue
 
 for lp1 in range(count):
-    LED.led14(interval)
-    LED.led15(interval)
-    LED.led14and15(interval)
+    LED_PIN.led1(interval, 14)
+    LED_PIN.led2(interval, 15)
+    LED_PIN.led1and2(interval, 14, 15)
 
 print (u"終わり")
