629dd9c9c86ed98592524a0579f53b2b48a98ef3
gaadesuo <peppu4@gmail.com>
2017-04-04 11:16:08 +0900

    pip修正

--- src/push_sw.py
+++ src/push_sw.py
@@ -2,12 +2,12 @@
 __author__ = "gaa"
 __date__ = '$2017/04/0 :10:57$'
 
-import gpio_setting as SETTING
+import gpio_setting as GPIO
 import time
 
 print(u"押すなよ。絶対に押すなよ！")
 while True:
-    if SETTING.push_sw(12):
+    if (GPIO.push_sw(12)):
         break
     else:
         continue
