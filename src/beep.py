567c601181fceafbd8294549f12eae125b511dbf
gaadesuo <peppu4@gmail.com>
2017-04-08 07:54:43 +0900

    pip修正

--- src/beep.py
+++ src/beep.py
@@ -10,15 +10,15 @@ __date__ = "$2017/02/24 19:10:13$"
 import wiringpi as GPIO
 from time import sleep
 
-#-----GPIOセットアップ-----
+# -----GPIOセットアップ-----
 
-buzzer = 26        #左19
+buzzer = 26        # 左19
 
 GPIO.wiringPiSetupGpio()
 GPIO.softToneCreate(buzzer)
 
 
-#-----音階周波数-----
+# -----音階周波数-----
 lsi = 247
 do = 262
 re = 294
@@ -27,22 +27,20 @@ fa = 349
 so = 392
 ra = 440
 si = 494
-do = 523
+hdo = 523
 
 
-#-----楽譜date-----
+# -----楽譜date-----
 onpu = (re,re,do,lsi,re,so,ra,si,si,si,ra,so,mi,mi,mi,fa,so,fa,so,mi,re,mi,re,lsi,re,re,re,re,do,lsi,re,so,ra,si,si,si,ra,so,ra,ra,ra,ra,so,so,fa,fa,so,so,so)
-mtime = (3,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,1,2,1,2,1,2,1,1,1,1)
+mtime = (2,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,1,2,1,2,1,2,1,1,1,1)
 stime = (1,1,1,1,1,1,1,1,1,1,1,8,1,1,1,1,1,1,1,1,1,1,1,1,8,1,1,1,1,1,1,1,1,1,1,1,1,8,1,1,1,1,1,1,1,1,4,4,4)
 
 if __name__ == "__main__":
 
+    # -----演奏-----
 
-    #-----演奏-----
-
-
-    for (yodo,mero,stp) in zip(onpu,mtime,stime):
-        GPIO.softToneWrite(buzzer,yodo)
+    for (yodo, mero, stp) in zip(onpu, mtime, stime):
+        GPIO.softToneWrite(buzzer, yodo)
         sleep(mero / 10)
-        GPIO.softToneWrite(buzzer,0)
+        GPIO.softToneWrite(buzzer, 0)
         sleep(stp / 10)
