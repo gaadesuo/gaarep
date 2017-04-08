f0fafc3505baafbbb3383b2460c6ffa8ab552b3d
gaadesuo <peppu4@gmail.com>
2017-04-08 09:50:05 +0900

    pip修正

--- src/test.py
+++ src/test.py
@@ -51,7 +51,7 @@ class Buzzer:
 
 if __name__ == "__main__":
 
-    BUZ = Buzzer(26, 262, 10, 0)
-    re = Buzzer(26, 294, 10, 0)
+    BUZ = Buzzer(26, 262, 10, 1)
+    re = Buzzer(26, 294, 10, 1)
     BUZ.beep()
     re.beep()
