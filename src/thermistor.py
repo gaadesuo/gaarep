cd49059de5e0bf3e21ed2d1cbec56765ad3ff3c1
gaadesuo <peppu4@gmail.com>
2017-04-22 11:21:00 +0900

    pip修正

--- src/thermistor.py
+++ src/thermistor.py
@@ -80,10 +80,10 @@ if __name__ == '__main__':
     try:
         while True:
             val_num = readadc(0, CLK, MOSI, MISO, CS)
-            inp_val = val_num * 3.3 / 1023
+            inp_val = val_num * 0.0008056640625
             print("{:.3f}".format(inp_val))
-            X = ((3.3 - inp_val) / 3.3) * 10000
-            Y = math.log( float(X) / float(10000) / float(3435))
+            X = ((3.3 - inp_val) / inp_val) * 10000
+            Y = math.log(float(X) / float(10000) / float(3435))
             Z = 1 / (float(25) + 273.0)
             Temp = (1 / (Y + Z)) - 273
 
