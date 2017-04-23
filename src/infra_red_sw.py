51d3f777c962aa04874e0c59e235457c809c1405
gaadesuo <peppu4@gmail.com>
2017-04-23 22:55:43 +0900

    pip修正

---
+++ src/infra_red_sw.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+__author__ = "gaa"
+__date__ = '$2017/04/23 :22:44$'
+
+import gpio_setting as GPIO
+import RPi.GPIO as PIN
+import time
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+    while True:
+        if GPIO.push_sw_1(5):
+            LED = GPIO.Led(21, 1)
+            LED.flash()
+            LED = GPIO.Led(21, 2)
+            LED.flash()
+        else:
+            time.sleep(0.1)
