5cdf281fe73af5de7839b79c74e83e58fc2e037f
gaadesuo <peppu4@gmail.com>
2017-05-12 19:18:45 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/test.py
+++ src/test.py
@@ -2,12 +2,4 @@
 __author__ = "gaa"
 __date__ = '$2017/04/11 :18:36$'
 
-# 辞書の中にキーと集合を入れる
-neler = {
-        "vipper": {"やる夫", "やらない夫", "できる夫"},
-        "nyusoku": {"やる夫", "できる夫", "やる実", "できる子"},
-        "women": {"やる実", "やらない子","できる子"},
-        "nanJ": {"やきう", "原住民", "やらない夫", "やらない子"}}
-
-print([ita for ita, name in neler.items() if "やる夫" not in name])
-print("ok")
+things = ["mozzarella", "cinderella", "salmone
