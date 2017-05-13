522e0c8875bd2d0b4c19638e818118c6d1002e23
gaadesuo <peppu4@gmail.com>
2017-05-13 23:51:50 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/test.py
+++ src/test.py
@@ -2,5 +2,4 @@
 __author__ = "gaa"
 __date__ = '$2017/04/11 :18:36$'
 
-things = ["mozzarella", "cinderella", "salmone"]
-print(things)
+print([(num_2, num_3) for num_2 in range(0, 2) for num_3 in range(0, 3)])
