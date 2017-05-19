4e7b645f71c66017cf6e7767be2ec2057de3e2c8
gaadesuo <peppu4@gmail.com>
2017-05-19 10:50:01 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/test.py
+++ src/test.py
@@ -2,4 +2,10 @@
 __author__ = "gaa"
 __date__ = '$2017/04/11 :18:36$'
 
-print([(num_2, num_3) for num_2 in range(0, 2) for num_3 in range(0, 3)])
+
+def neler(**men):
+    print(men)
+
+
+neler(vipper="やる夫", women="やる実")
+
