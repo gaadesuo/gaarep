c0a496bb610007561fdb3001395f601f3c0c2fd3
gaadesuo <peppu4@gmail.com>
2017-05-24 12:19:25 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/paiza.py
+++ src/paiza.py
@@ -17,3 +17,20 @@ if __name__ == '__main__':
         list.append(num)
     list2 = map(str, list)
     print(" ".join(list2))
+
+    # paiza暗号化複合化
+    ans_list = []
+    nums = input().split()
+    keys = [str(key) for key in range(10)]
+    enc_dict = {k: n for k, n in zip(keys, nums)}
+    word = input()
+    c_num = [num for num in input()]
+    if word.startswith("enc"):
+        ans_list = [enc_dict[i] for i in c_num]
+        print("".join(ans_list))
+    else:
+        for i in c_num:
+            for ans_key, ans_val in enc_dict.items():
+                if i == ans_val:
+                    ans_list.append(ans_key)
+    print("".join(ans_list))
