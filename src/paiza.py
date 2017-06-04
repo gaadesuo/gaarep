b41ea0747b68e46ac6f173f12c390a02fecad3d7
gaadesuo <peppu4@gmail.com>
2017-06-04 20:11:49 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/paiza.py
+++ src/paiza.py
@@ -34,3 +34,60 @@ if __name__ == '__main__':
                 if i == ans_val:
                     ans_list.append(ans_key)
     print("".join(ans_list))
+
+    ans_list2 = []
+    num = int(input())
+    up_list = [int(i) for i in input().split()]
+    low_list = [int(i) for i in input().split()]
+
+    for m in range(num):
+        ans_list = []
+        for n in range(num):
+            ans = low_list[m] + up_list[n]
+            ans_list.append(str(ans))
+        ans_list2.append(ans_list)
+
+    for o in range(num):
+        print(" ".join(ans_list2[o]))
+
+    # その２
+
+    p_list = []
+    b_list = []
+    # キャンペーン商品数と割引設定入力
+
+    # 入力される値は割引商品の種類数
+    for lp0 in range(int(input())):
+        # 入力される値は[0]から単価 割引される個数 割引額
+        price = [int(lp1) for lp1 in input().split()]
+        # print(price)
+        p_list.append(price)
+    # print(p_list)
+
+    # 購入種類と数の入力
+
+    # 入力される値は購入種類数
+    for lp2 in range(int(input())):
+        # 入力される値は[0]から製品NO 購入数
+        buy = [int(lp3) for lp3 in input().split()]
+        # print(buy)
+        b_list.append(buy)
+    # print(b_list)
+
+    # 処理
+
+    for lp4 in range(lp2 + 1):
+        kind = (b_list[lp4][0] - 1)
+        discount = b_list[lp4][1] / p_list[kind][1]
+        if 0 < int(discount):
+            print((p_list[kind][0] * b_list[lp4][1]) - (p_list[kind][2] * int(discount)))
+        else:
+            print(p_list[kind][0] * b_list[lp4][1])
+
+    # その３
+    hi_list = ["200"]
+    low_list = ["100"]
+    for lp1 in range(int(input())):
+        high = input().split()
+        hi_list.append(high[1]) if high[0] == "le" else low_list.append(high[1])
+    print("{} {}".format(max(low_list), min(hi_list)))
