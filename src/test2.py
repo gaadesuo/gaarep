82548c793675ceadf08c1a23daa156d8741fb927
gaadesuo <peppu4@gmail.com>
2017-11-25 11:57:25 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/test2.py
+++ src/test2.py
@@ -1,35 +1,66 @@
-kbn = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
-kbh = "qwertasdfgzxcvb"
-kbm = "yuiophjklnm"
+import numpy as np
 
-inp = input()
-#inp = "lkfhakmwnleighlaid"
-rst = 0
-bfr = inp[0]
-if  (kbh.find(bfr) != -1): typ = 0
-elif(kbm.find(bfr) != -1): typ = 1
-#print(inp, bfr, typ)
 
-for trg in inp[1:]:
-    if  (kbh.find(trg) != -1): hmp = 0
-    elif(kbm.find(trg) != -1): hmp = 1
-    if  (kbn[0].find(trg) != -1): y = 0
-    elif(kbn[1].find(trg) != -1): y = 1
-    elif(kbn[2].find(trg) != -1): y = 2
-    x = kbn[y].find(trg)
-    z = len(kbn[y])-1
+def word_check():
+    """
+    """
+    if check_word in all_word[0]:
+        l_grid_no = [all_word[0].find(check_word), 0]
+        return l_grid_no
+    elif check_word in all_word[1]:
+        l_grid_no = [all_word[1].find(check_word), 1]
+        return l_grid_no
+    else:
+        l_grid_no = [all_word[2].find(check_word), 2]
+        return l_grid_no
 
-   rin = kbn[y+0][x+0]
-    if((x-1)>=0): rin = rin + kbn[y+0][x-1]
-    if((x+1)<=z): rin = rin + kbn[y+0][x+1]
-    if(((y-1)>=0) and ((len(kbn[y-1])-1) >= x)): rin = rin + kbn[y-1][x+0]
-    if(((y+1)<=2) and ((len(kbn[y+1])-1) >= x)): rin = rin + kbn[y+1][x+0]
 
-#    print(typ, hmp, bfr, rin)
+all_word = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+all_word_l = "qwertasdfgzxcvb"
+all_word_r = "yuiophjklnm"
+word_l = "tgb"
+word_r = "yhn"
 
-   if(typ != hmp):
-        if(rin.find(bfr) != -1): rst = rst + 1
-        else                   : typ = hmp
+grid_0 = [0, 0]
+grid_1 = [0, 0]
+lr_hand = 0
+counter = 0
 
-   bfr = kbn[y][x]
-print(rst)
+inp_word = list(input())
+for check_word in inp_word:
+    # 前回のキータッチでflagが立っているかの確認
+    if grid_0[0] == 0 and grid_0[1] == 0:
+        # 左手のリミットを触れた時
+        if check_word in word_l:
+            lr_hand = 1
+            grid_0 = word_check()
+        # 右手のリミットを触れたとき
+        elif check_word in word_r:
+            lr_hand = 2
+            grid_0 = word_check()
+
+    # 前回flagが立ったままの場合
+    else:
+        grid_1 = word_check()
+        # print("フラグが立ったので座標表示。前回の座標:{} 今回の座標{}".format(grid_0, grid_1))
+        np_grid_0 = np.array(grid_0)
+        np_grid_1 = np.array(grid_1)
+        ans_grid = np_grid_0 - np_grid_1
+        # キーが隣合わせかの確認 隣り合合わせなら無条件で座標入れ替え
+        if (ans_grid[0] == -1 or ans_grid[0] == 0 or ans_grid[0] == 1) and (
+                    ans_grid[1] == -1 or ans_grid[1] == 0 or ans_grid[1] == 1):
+            # 左手の時右側のキーならカウントアップ
+            if lr_hand == 1:
+                if check_word in all_word_r:
+                    counter += 1
+            # 右手の時左側のキーならカウントアップ
+            else:
+                if check_word in all_word_l:
+                    counter += 1
+            grid_0 = grid_1
+
+        else:
+            grid_0 = [0, 0]
+            lr_hand = 0
+
+print(counter)
--- src/test3.py
+++ src/test3.py
@@ -8,35 +8,36 @@ inp_word = list(input())
 counter = 0
 grid_0 = [0, 0]
 grid_1 = [0, 0]
-for chake_word in inp_word:
+for check_word in inp_word:
     # 前回間違えぎりぎりのフラグが立ってない場合
     if grid_0[0] == 0 and grid_0[1] == 0:
-        if chake_word in side_limit:
+        if check_word in side_limit:
             # 間違えぎりぎりをタッチしたので文字からx, y座標を求める
-            if chake_word in all_word[0]:
-                grid_0 = [all_word[0].find(chake_word), 0]
-            elif chake_word in all_word[1]:
-                grid_0 = [all_word[0].find(chake_word), 1]
+            if check_word in all_word[0]:
+                grid_0 = [all_word[0].find(check_word), 0]
+            elif check_word in all_word[1]:
+                grid_0 = [all_word[0].find(check_word), 1]
             else:
-                grid_0 = [all_word[0].find(chake_word), 2]
+                grid_0 = [all_word[0].find(check_word), 2]
 
     # 前回間違えのフラグが立ってる場合
     else:
-        # print("flagが立ってるため前回の座標チェック x:{:0d}, y:{:0d}".format(grid_0[0], grid_0[1]))
+        print("flagが立ってるため前回の座標チェック x:{:0d}, y:{:0d}".format(grid_0[0], grid_0[1]))
         # 今回の座標取得
-        if chake_word in all_word[0]:
-            grid_1 = [all_word[0].find(chake_word), 0]
-        elif chake_word in all_word[1]:
-            grid_1 = [all_word[0].find(chake_word), 1]
+        if check_word in all_word[0]:
+            grid_1 = [all_word[0].find(check_word), 0]
+        elif check_word in all_word[1]:
+            grid_1 = [all_word[0].find(check_word), 1]
         else:
-            grid_1 = [all_word[0].find(chake_word), 2]
+            grid_1 = [all_word[0].find(check_word), 2]
 
         # 座標が前回と近似値かどうかの確認。近似値ならカウントしgrid_0上書き。違う場合はgrid_0初期化
         np_grid_0 = np.array(grid_0)
         np_grid_1 = np.array(grid_1)
         diff_grid = np_grid_0 - np_grid_1
-        # print("前回との座標の差異 x:{:0d}, y:{:0d}".format(diff_grid[0], diff_grid[1]))
-        if -1 <= diff_grid[0] <= 1 or -1 <= diff_grid[1] <= 1:
+        print("前回との座標の差異 x:{:0d}, y:{:0d}".format(diff_grid[0], diff_grid[1]))
+        if (-1 <= diff_grid[0] <= 1 and diff_grid[1] == 0)\
+                or (-1 <= diff_grid[1] <= 1 and diff_grid[0] == 0):
             counter += 1
             grid_0 = grid_1
         else:
@@ -44,3 +45,5 @@ for chake_word in inp_word:
 
 print(counter)
 
+# 右手と左手判別
+右手が左に行った後みぎてにもどったらかうんとしない
