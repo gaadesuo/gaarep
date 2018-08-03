e1f12e8b3094134317b33a01188675fc0cd8c465
gaadesuo <peppu4@gmail.com>
2018-08-04 02:40:17 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

---
+++ src/oreilly/auto/3_11_1_collatz.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+__author__ = "gaa"
+__date__ = '2018/08/04 02:24'
+
+
+def collatz(number):
+    """
+    コラッツ数列
+    :param number: 入力した数字
+    :return:
+    """
+    num = int(number / 2 if number % 2 == 0 else 3 * number + 1)
+    return num
+
+
+def main():
+    try:
+        num = int(input("整数を入力 >>> "))
+        while num != 1:
+            num = collatz(num)
+            print(num)
+    except ValueError:
+        print("整数を入れてください")
+
+
+if __name__ == '__main__':
+    main()
