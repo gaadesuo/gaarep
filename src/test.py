f6d5b4ce69f45cd2670ae1e32e81826e4d386e6a
gaadesuo <peppu4@gmail.com>
2018-11-02 20:28:37 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/test.py
+++ src/test.py
@@ -1,18 +1,12 @@
-grid = [['.', '.', '.', '.', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['.', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
+the_board = {"top_L": " ", "top_M": " ", "top_R": " ",
+             "mid_L": " ", "mid_M": " ", "mid_R": " ",
+             "low_L": " ", "low_M": " ", "low_R": " "}
 
-new_grid = [[], [], [], [], [], []]
-for x in range(6):
-    for y in range(9):
-        # print(x, y)
-        new_grid[x].append(grid[y][x])
-# print(new_grid)
-for i in new_grid:
-    print("".join(i))
+print("{}|{}|{}".format(the_board["top_L"], the_board["top_M"], the_board["top_R"]))
+print("-+-+-")
+print("{}|{}|{}".format(the_board["mid_L"], the_board["mid_M"], the_board["mid_R"]))
+print("-+-+-")
+print("{}|{}|{}".format(the_board["low_L"], the_board["low_R"], the_board["low_R"]))
+
+if the_board["top_L"] == the_board["top_M"] == the_board["top_R"] == "o"or\
+        the_board["top_L"] == the_board["top_M"] == the_board["top_R"] == "x":
