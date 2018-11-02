bb2a58ae2810dc974608fc23706107610e743859
gaadesuo <peppu4@gmail.com>
2018-11-02 20:56:36 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/test.py
+++ src/test.py
@@ -9,4 +9,21 @@ print("-+-+-")
 print("{}|{}|{}".format(the_board["low_L"], the_board["low_R"], the_board["low_R"]))
 
 if the_board["top_L"] == the_board["top_M"] == the_board["top_R"] == "o"or\
-        the_board["top_L"] == the_board["top_M"] == the_board["top_R"] == "x":
+    the_board["mid_L"] == the_board["mid_M"] == the_board["mid_R"] == "o" or\
+    the_board["low_L"] == the_board["low_M"] == the_board["low_R"] == "o" or\
+    the_board["top_L"] == the_board["mid_L"] == the_board["low_L"] == "o" or\
+    the_board["top_M"] == the_board["mid_M"] == the_board["low_M"] == "o" or\
+    the_board["top_R"] == the_board["mid_R"] == the_board["low_R"] == "o" or\
+    the_board["top_L"] == the_board["mid_M"] == the_board["low_R"] == "o" or\
+        the_board["top_R"] == the_board["mid_M"] == the_board["low_L"] == "o":
+    print()
+
+elif the_board["top_L"] == the_board["top_M"] == the_board["top_R"] == "x" or\
+    the_board["mid_L"] == the_board["mid_M"] == the_board["mid_R"] == "x" or\
+    the_board["low_L"] == the_board["low_M"] == the_board["low_R"] == "x" or\
+    the_board["top_L"] == the_board["mid_L"] == the_board["low_L"] == "x" or\
+    the_board["top_M"] == the_board["mid_M"] == the_board["low_M"] == "x" or\
+    the_board["top_R"] == the_board["mid_R"] == the_board["low_R"] == "x" or\
+    the_board["top_L"] == the_board["mid_M"] == the_board["low_R"] == "x" or\
+        the_board["top_R"] == the_board["mid_M"] == the_board["low_L"] == "x":
+    print()
