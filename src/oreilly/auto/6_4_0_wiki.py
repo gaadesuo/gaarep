5a0215c5b1367ace1bfafdd2919b95158ea611bf
gaadesuo <peppu4@gmail.com>
2018-11-10 11:52:57 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/oreilly/auto/6_4_0_wiki.py
+++ src/oreilly/auto/6_4_0_wiki.py
@@ -3,9 +3,20 @@
 __author__ = "gaa"
 __date__ = '2018/11/10 09:58'
 
+import pyperclip
+
 
 def main():
-    pass
+    new_list = []
+    word = pyperclip.paste()
+    # print(word)
+    word_list = word.split("\n")
+    # print(word_list)
+    for s in word_list:
+        new_list.append("* " + s)
+    # print(new_list)
+    new_word = "\n".join(new_list)
+    pyperclip.copy(new_word)
 
 
 if __name__ == '__main__':
---
+++ src/oreilly/auto/6_7_1_make_table.py
@@ -0,0 +1,51 @@
+#! python3
+# -*- coding: utf-8 -*-
+__author__ = "gaa"
+__date__ = '2018/11/10 10:59'
+
+
+def print_table(list):
+    """
+    各リストの要素を後ろ合わせで縦に表記する
+    :param list: 表示する文字列のリスト
+    """
+    num_list = []
+    new_list = []
+    print_list = [[], [], [], []]
+
+    # リストの中の最大文字数に合わせ右揃えにする
+    for l in list:
+        for w in l:
+            num_list.append(len(w))
+        # print(num_list)
+        m_num = max(num_list)
+        for i in range(len(l)):
+            l[i] = l[i].rjust(m_num)
+        # print(l)
+        new_list.append(l)
+        num_list = []
+    # print(new_list)
+
+    # リストの要素を表記順に並べなおす
+    for i_0 in range(len(new_list)):
+        for i_1 in range(len(new_list[i_0])):
+            # print(new_list[i_0][i_1])
+            print_list[i_1].append(new_list[i_0][i_1])
+            # print(print_list)
+
+    # 表示
+    for l in print_list:
+        print(" ".join(l))
+
+
+def main():
+
+    table_date = [["apples", "oranges", "cherries", "banana"],
+                  ["Alice", "Bob", "Carol", "David"],
+                  ["dogs", "cats", "moose", "goose"]]
+
+    print_table(table_date)
+
+
+if __name__ == '__main__':
+    main()
