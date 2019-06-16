d470f24574a8cba4878431b23202de32dd7a74eb
gaadesuo <peppu4@gmail.com>
2019-06-16 14:13:00 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.py

--- src/oreilly/auto/test.py
+++ src/oreilly/auto/test.py
@@ -1,29 +1,7 @@
-# -*- coding: utf-8 -*-
-__author__ = "gaa"
-__date__ = '2018/08/04 17:26'
+import os
 
-import re
-import pyperclip
+file_name = ["hogehoge.txt", "piyopiyo.exe"]
 
-phone_regex = re.compile(r"""(
-(\d{1,4}|\(\d{1,4}\)) # 市外局番
-(-|\s)
-(\d{1,4})             # 市内局番
-(-|\s)
-(\d{3,4})             # 加入者番号
-)""", re.VERBOSE)
-
-mail_regex = re.compile(r"""(
-[a-zA-Z0-9._%+-]+ # ユーザー名
-@
-[a-zA-Z0-9.-]+    # ドメイン
-(\.[a-zA-Z{2,4}]+)      # ドットなんとか
-)""", re.VERBOSE)
-
-text = str(pyperclip.paste())
-print(text)
-
-phone_list = phone_regex.findall(text)
-mail_list = mail_regex.findall(text)
-
-print(phone_list, mail_list)
+for name in file_name:
+    path = os.path.join("C:\Python34", name)
+    print(path)
